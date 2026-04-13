fb1c1bb3875e152cf4d5ab91da08394251dcf183
moamen <mo@menbasel.com>
2026-04-13 05:19:59 +0200

    Add phosphor cask formula v1.0.0

---
+++ Casks/phosphor.rb
@@ -0,0 +1,18 @@
+cask "phosphor" do
+  version "1.0.0"
+  sha256 "edfdfb481eeb15846026cc66104329ac039db5736371de32801a02dbf29ea42c"
+
+  url "https://github.com/momenbasel/Phosphor/releases/download/v#{version}/Phosphor.dmg"
+  name "Phosphor"
+  desc "Free and open-source iOS device manager for macOS"
+  homepage "https://github.com/momenbasel/Phosphor"
+
+  depends_on macos: ">= :sonoma"
+
+  app "Phosphor.app"
+
+  zap trash: [
+    "~/Library/Caches/com.phosphor.app",
+    "~/Library/Preferences/com.phosphor.app.plist",
+  ]
+end
