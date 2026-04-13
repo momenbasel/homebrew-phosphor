6a568398ee5608346d7f580b3763d65ea8efcbcb
moamen <mo@menbasel.com>
2026-04-13 22:03:28 +0200

    Update SHA - DMG with Applications symlink

--- Casks/phosphor.rb
+++ Casks/phosphor.rb
@@ -1,6 +1,6 @@
 cask "phosphor" do
   version "1.0"
-  sha256 "9d3c0c4000a67dd22e97ce3a6ebb00d7c5dca022293f68e8e4fa0d1d44073123"
+  sha256 "16483b52e47682e041236590115b2bb6cd31ab37aec4687d90d6470fa3adc169"
 
   url "https://github.com/momenbasel/Phosphor/releases/download/v#{version}/Phosphor.dmg"
   name "Phosphor"
