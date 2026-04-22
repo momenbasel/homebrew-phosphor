7a6e00c8ab28070f4b29fae25cdbbab40020f6be
moamen <mo@menbasel.com>
2026-04-22 02:33:24 +0200

    phosphor: bump to 1.0.1
    iOS 26 backup diagnostics release.
    - Fix iOS 26 backup failure with hidden stderr (#1)
    - Widen pymobiledevice3 binary discovery
    - Translate backup stderr into actionable hints

--- Casks/phosphor.rb
+++ Casks/phosphor.rb
@@ -1,6 +1,6 @@
 cask "phosphor" do
-  version "1.0"
-  sha256 "3d2f6e1303ea1e42e69ac442b88a61e0b768b3307001e03cb7a7df288666f147"
+  version "1.0.1"
+  sha256 "f64a59177eaa714293aaea62f9e38dcc0f62995f33deac66a4eddcaaa000a284"
 
   url "https://github.com/momenbasel/Phosphor/releases/download/v#{version}/Phosphor.dmg"
   name "Phosphor"
